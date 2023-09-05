2913e500eacdcb4a74adf21d3b167ded22572f0f
Rosa Yan <rosa012933@gmail.com>
2023-09-05 11:02:25 +1000

    Update settings.py

--- backend/syncTalk/syncTalk/settings.py
+++ backend/syncTalk/syncTalk/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o752h6)+hv+wdxjoa2-cqtov(4e47!)sx6h60o*@*sl2e0zyd='
+#Secret key removed for security reasons
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
