8fe4c02bf903478ff82eb0f1ae093364c639fc5c
Jliew1113-Desktop <shanmou@rocketmail.com>
2023-09-10 17:27:35 +1000

    center everything

--- frontend/sync-talk/src/pages/Loading.js
+++ frontend/sync-talk/src/pages/Loading.js
@@ -5,7 +5,7 @@ import './css/Loading.css';
 const Loading = () => {
     return (
         <div className="Loading" >
-            <img className="Infographic" src="https://via.placeholder.com/500x500" />
+            <img className="Infographic" src="https://via.placeholder.com/600x600" />
             <div className="Title">
                 <p>Generating</p>
                 <ReactLoading type="bubbles" color="#1D3557" height={100} width={200} />
