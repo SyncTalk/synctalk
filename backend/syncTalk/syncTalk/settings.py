34a09ebefd09304d698784a616b2ef0c39e7bf8e
Rosa Yan <rosa012933@gmail.com>
2023-09-10 19:13:35 +1000

    decouple secret key from settings

--- backend/syncTalk/syncTalk/settings.py
+++ backend/syncTalk/syncTalk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-#Secret key removed for security reasons
+#Secret moved to env
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
