efc90f04896ae14b850d1c4c1ecd3b5a664fbe44
Jliew1113-Desktop <shanmou@rocketmail.com>
2023-09-17 15:04:43 +1000

    dynamic button link

--- frontend/sync-talk/src/components/Navbar.js
+++ frontend/sync-talk/src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { HomeBtnLink, Nav, NavLeft, NavRight, UploadBtnLink } from './NavBarElements';
+import { Nav, NavLeft, NavRight, HomeBtnLink, UploadBtnLink, DownloadBtnLink, AboutBtnLink } from './NavBarElements';
 
-const Navbar = () => {
+const Navbar = ({ page }) => {
+    let rightButton;
+
+    switch (page) {
+        case 'result':
+            rightButton = <DownloadBtnLink />;
+            break;
+        case 'about':
+            rightButton = <UploadBtnLink />;
+            break;
+        default:
+            rightButton = <AboutBtnLink />;
+    }
+
     return (
         <>
             <Nav>
@@ -9,7 +22,7 @@ const Navbar = () => {
                     <HomeBtnLink />
                 </NavLeft>
                 <NavRight>
-                    <UploadBtnLink />
+                    {rightButton}
                 </NavRight>
             </Nav>
         </>
@@ -17,5 +30,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
