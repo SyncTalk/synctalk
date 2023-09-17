d8cf777405d54d56b180a394b83d1ccf9ee0b6bc
QingyueZhu <129928299+QingyueZhu@users.noreply.github.com>
2023-09-17 14:46:53 +0800

    Delete src directory

--- src/HomePage.js
+++
@@ -1,8 +0,0 @@
-// Qingyue Zhu
-import React from 'react';
-
-const HomePage = () => {
-  return <div>Home Page Content</div>;
-};
-
-export default HomePage;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-// Qingyue Zhu
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AppRouter from './AppRouter';
-import './App.css';
-
-const root = createRoot(document.getElementById('root'));
-
-root.render(<AppRouter></AppRouter>)
