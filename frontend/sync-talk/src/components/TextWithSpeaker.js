ec83287bcd54d64fafe85e374b4ce58f877c6f6e
Jliew1113-Desktop <shanmou@rocketmail.com>
2023-09-19 02:02:19 +1000

    add transaltion tool tip

--- frontend/sync-talk/src/components/TextWithSpeaker.js
+++ frontend/sync-talk/src/components/TextWithSpeaker.js
@@ -7,6 +7,7 @@ const TextWithSpeaker = ({ text, startTime, endTime }) => {
     const [isPlaying, setIsPlaying] = useState(false);
     const [isClicked, setIsClicked] = useState(false);
     const [isCurrent, setIsCurrent] = useState(false);
+    const [selectedWord, setSelectedWord] = useState(null);
 
     const handleAudioClick = () => {
         const audio = document.getElementById('audio');
@@ -16,6 +17,45 @@ const TextWithSpeaker = ({ text, startTime, endTime }) => {
         audio.play();
         setIsPlaying(true);
     };
+
+    async function getTranslation(word) {
+        
+        const axios = require('axios').default;
+        const { v4: uuidv4 } = require('uuid');
+        const apiKey = '6e712e735c384f7a99f0055f2ce90fce';
+        const location = 'australiaeast'; // e.g., 'eastus' or 'westus'
+        const endpoint = "https://api.cognitive.microsofttranslator.com";
+    
+        try {
+            const response = await axios({
+            baseURL: endpoint,
+            url: '/translate',
+            method: 'post',
+            headers: {
+                'Ocp-Apim-Subscription-Key': apiKey,
+                // location required if you're using a multi-service or regional (not global) resource.
+                'Ocp-Apim-Subscription-Region': location,
+                'Content-type': 'application/json',
+                'X-ClientTraceId': uuidv4().toString()
+            },
+            params: {
+                'api-version': '3.0',
+                'from': 'en',
+                'to': ['fr', 'zu']
+            },
+            data: [{
+                'text': word
+            }],
+            responseType: 'json'
+            });
+            const data = await response.json();
+            console.log(JSON.stringify(response.data, null, 4));
+            return data[0].translations[0].text;
+        } catch (error) {
+            console.error('Error translating text:', error);
+        }
+        };
+
 
     useEffect(() => {
         const audio = document.getElementById('audio');
@@ -50,14 +90,33 @@ const TextWithSpeaker = ({ text, startTime, endTime }) => {
         }
     }, [currentTime, startTime, endTime]);
 
+
+    const handleWordClick = async (word) => {
+        // Call an API to get the translation of the word
+        const translation = await getTranslation(word);
+
+        // Set the selected word and its translation in the state
+        setSelectedWord({ word, translation });
+    };
+
     return (
         <div className="TextWithSpeaker">
-            <span className={`Text ${isCurrent ? 'Playing' : ''}`}>{text}</span>
-            <button className="SpeakerButton" onClick={handleAudioClick}>
-                <FontAwesomeIcon icon={faVolumeUp} />
-            </button>
+          <span className={`Text ${isCurrent ? 'Playing' : ''}`}>
+            {text.split(' ').map((word, index) => (
+              <span
+                key={index}
+                onClick={() => handleWordClick(word)}
+                title={selectedWord?.word === word ? selectedWord.translation : ''}
+              >
+                {word}{' '}
+              </span>
+            ))}
+          </span>
+          <button className="SpeakerButton" onClick={handleAudioClick}>
+            <FontAwesomeIcon icon={faVolumeUp} />
+          </button>
         </div>
-    );
+      );
 };
 
 export default TextWithSpeaker;
