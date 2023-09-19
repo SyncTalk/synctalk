811530148fb19081c8953ddce65cbf6cde8530a3
ilanrathner <ilan.rathner@gmail.com>
2023-09-19 20:10:42 +1000

    changed split text to split on all punctuation. added comments

--- backend/syncTalk/syncTalk/settings.py
+++ backend/syncTalk/syncTalk/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'PyPDF2',
     'docx',
+    'ffmpeg',
     'rest_framework',
-    'syncTalk_app'
+    'syncTalk_app',
 ]
 
 MIDDLEWARE = [
