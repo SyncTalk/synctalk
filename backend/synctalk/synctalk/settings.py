2682097389812164e380af4ad49eedcdc2bff710
angwarrenn <ang.warrenn@gmail.com>
2023-09-21 02:55:25 +1000

    update ALLOWED_HOSTS

--- backend/synctalk/synctalk/settings.py
+++ backend/synctalk/synctalk/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-o752h6)+hv+wdxjoa2-cqtov(4e47!)sx6h60o*@*sl2e0zyd=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['170.64.196.122']
 
 
 # Application definition
