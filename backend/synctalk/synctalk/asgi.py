79f8af2d54f71df540244115dea108fb6433ff5b
angwarrenn <ang.warrenn@gmail.com>
2023-09-20 23:02:06 +1000

    fix backend file structure

--- backend/synctalk/synctalk/asgi.py
+++ backend/synctalk/synctalk/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for syncTalk project.
+ASGI config for synctalk project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syncTalk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'synctalk.settings')
 
 application = get_asgi_application()
