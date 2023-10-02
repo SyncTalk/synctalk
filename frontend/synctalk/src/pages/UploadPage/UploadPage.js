59e39f11ad6ee080ea829f99b16d338890d2b720
QingyueZhu <129928299+QingyueZhu@users.noreply.github.com>
2023-10-03 07:59:25 +0800

    Allow MP3 and PDF file uploads

--- frontend/synctalk/src/pages/UploadPage/UploadPage.js
+++ frontend/synctalk/src/pages/UploadPage/UploadPage.js
@@ -1,48 +1,52 @@
-import React, { useState } from "react";
-import axios from "axios";
-import uploadIcon from "../../assets/upload-icon.jpg";
-import "../UploadPage/UploadPage.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../UploadPage/UploadPage.css';
+import uploadIcon from '../../assets/upload-icon.jpg';
 
 const UploadPage = () => {
-  const [selectedMp3Mp4File, setSelectedMp3Mp4File] = useState(null);
+  const [selectedMp3File, setSelectedMp3File] = useState(null);
   const [selectedDocxTxtFile1, setSelectedDocxTxtFile1] = useState(null);
   const [selectedDocxTxtFile2, setSelectedDocxTxtFile2] = useState(null);
-  const [selectedMp3Mp4FileName, setSelectedMp3Mp4FileName] = useState("");
-  const [selectedDocxTxtFileName1, setSelectedDocxTxtFileName1] = useState("");
-  const [selectedDocxTxtFileName2, setSelectedDocxTxtFileName2] = useState("");
+  const [selectedMp3FileName, setSelectedMp3FileName] = useState('');
+  const [selectedDocxTxtFileName1, setSelectedDocxTxtFileName1] = useState('');
+  const [selectedDocxTxtFileName2, setSelectedDocxTxtFileName2] = useState('');
 
-  const handleMp3Mp4FileSelect = (event) => {
+  const handleMp3FileSelect = (event) => {
     const file = event.target.files[0];
-    if (file && (file.type === "audio/mp3" || file.type === "video/mp4")) {
-      setSelectedMp3Mp4File(file);
-      setSelectedMp3Mp4FileName(file.name); // Set the file name
+    if (file && file.name.toLowerCase().endsWith('.mp3')) {
+      setSelectedMp3File(file);
+      setSelectedMp3FileName(file.name);
     } else {
-      alert("Please select a valid mp3 or mp4 file");
+      alert('Please select a valid mp3 file');
     }
   };
 
-  const isDocxOrTxtFile = (file) => {
+  const isDocxOrTxtOrPdfFile = (file) => {
     const fileName = file.name.toLowerCase();
-    return fileName.endsWith(".docx") || fileName.endsWith(".txt");
+    return (
+      fileName.endsWith('.docx') ||
+      fileName.endsWith('.txt') ||
+      fileName.endsWith('.pdf')
+    );
   };
 
   const handleDocxTxtFileSelect1 = (event) => {
     const file = event.target.files[0];
-    if (file && isDocxOrTxtFile(file)) {
+    if (file && isDocxOrTxtOrPdfFile(file)) {
       setSelectedDocxTxtFile1(file);
-      setSelectedDocxTxtFileName1(file.name); // Set the file name
+      setSelectedDocxTxtFileName1(file.name);
     } else {
-      alert("Please select a valid docx or txt file");
+      alert('Please select a valid docx, txt, or pdf file');
     }
   };
 
   const handleDocxTxtFileSelect2 = (event) => {
     const file = event.target.files[0];
-    if (file && isDocxOrTxtFile(file)) {
+    if (file && isDocxOrTxtOrPdfFile(file)) {
       setSelectedDocxTxtFile2(file);
-      setSelectedDocxTxtFileName2(file.name); // Set the file name
+      setSelectedDocxTxtFileName2(file.name);
     } else {
-      alert("Please select a valid docx or txt file");
+      alert('Please select a valid docx, txt, or pdf file');
     }
   };
 
@@ -50,24 +54,20 @@ const UploadPage = () => {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
     if (file) {
-      if (
-        fileType === "audio" &&
-        (file.type === "audio/mp3" || file.type === "video/mp4")
-      ) {
-        setSelectedMp3Mp4File(file);
-        setSelectedMp3Mp4FileName(file.name); // Set the file name
-      } else if (fileType === "text1" && isDocxOrTxtFile(file)) {
+      const fileExtension = file.name.toLowerCase().split('.').pop();
+      if (fileType === 'audio' && fileExtension === 'mp3') {
+        setSelectedMp3File(file);
+        setSelectedMp3FileName(file.name);
+      } else if (fileType === 'text1' && isDocxOrTxtOrPdfFile(file)) {
         setSelectedDocxTxtFile1(file);
-        setSelectedDocxTxtFileName1(file.name); // Set the file name
-      } else if (fileType === "text2" && isDocxOrTxtFile(file)) {
+        setSelectedDocxTxtFileName1(file.name);
+      } else if (fileType === 'text2' && isDocxOrTxtOrPdfFile(file)) {
         setSelectedDocxTxtFile2(file);
-        setSelectedDocxTxtFileName2(file.name); // Set the file name
+        setSelectedDocxTxtFileName2(file.name);
       } else {
-        alert(
-          `Please drop a valid ${
-            fileType === "audio" ? "mp3 or mp4" : "docx or txt"
-          } file`,
-        );
+        alert(`Please drop a valid ${
+          fileType === 'audio' ? 'mp3' : 'docx, txt, or pdf'
+        } file`);
       }
     }
   };
@@ -78,136 +78,109 @@ const UploadPage = () => {
 
   const isFileSelected = () => {
     return (
-      selectedMp3Mp4File !== null ||
-      selectedDocxTxtFile1 !== null ||
+      selectedMp3File !== null &&
+      selectedDocxTxtFile1 !== null &&
       selectedDocxTxtFile2 !== null
     );
   };
 
   const handleUpload = () => {
-    if (selectedMp3Mp4File && selectedDocxTxtFile1) {
+    if (selectedMp3File && selectedDocxTxtFile1 && selectedDocxTxtFile2) {
       const formData = new FormData();
-      formData.append("mp3Mp4File", selectedMp3Mp4File);
-      formData.append("docxTxtFile1", selectedDocxTxtFile1);
-
-      if (selectedDocxTxtFile2) {
-        formData.append("docxTxtFile2", selectedDocxTxtFile2);
-      }
+      formData.append('mp3File', selectedMp3File);
+      formData.append('docxTxtFile1', selectedDocxTxtFile1);
+      formData.append('docxTxtFile2', selectedDocxTxtFile2);
 
       axios
-        .post("/upload", formData, {
+        .post('/upload', formData, {
           headers: {
-            "Content-Type": "multipart/form-data",
+            'Content-Type': 'multipart/form-data',
           },
         })
         .then((response) => {
-          console.log("Upload successful", response.data);
-          // Handle successful upload here
+          console.log('Upload successful', response.data);
         })
         .catch((error) => {
-          console.error("Upload failed", error);
+          console.error('Upload failed', error);
         });
     } else {
-      alert("Please make sure to select the required files");
+      alert('Please make sure to select all three required files');
     }
   };
 
   return (
     <div>
-      <div className="nav-bar"></div>
-      <div className="title upload-title">Upload Your Files</div>
-      <div className="subtitle">
-        <div className="three-part">Audio</div>
-        <div className="three-part">Text</div>
-        <div className="three-part">Translation (Optional)</div>
+      <div className='nav-bar'></div>
+      <div className='title upload-title'>Upload Your Files</div>
+      <div className='subtitle'>
+        <div className='three-part'>Audio</div>
+        <div className='three-part'>Text</div>
+        <div className='three-part'>Translation</div>
       </div>
-      <div className="upload-instrument">
-        <div className="three-part">Supported formats: .mp3, .mp4</div>
-        <div className="three-part">Supported formats: .docx, .txt</div>
-        <div className="three-part">Supported formats: .docx, .txt</div>
+      <div className='upload-instrument'>
+        <div className='three-part'>Supported format: .mp3</div>
+        <div className='three-part'>Supported formats: .docx, .txt, .pdf</div>
+        <div className='three-part'>Supported formats: .docx, .txt, .pdf</div>
       </div>
       <div>
-        <div className="upload-square">
+        <div className='upload-square'>
           <div
-            className={`three-part ${isFileSelected() ? "file-selected" : ""}`}
-            onDrop={(event) => handleDrop(event, "audio")}
+            className={`three-part ${isFileSelected() ? 'file-selected' : ''}`}
+            onDrop={(event) => handleDrop(event, 'audio')}
             onDragOver={preventDefault}
           >
-            <img
-              className="upload-icon"
-              src={uploadIcon}
-              alt="upload feature icon"
-            ></img>
-            <div className="select-part">
+            <img className='upload-icon' src={uploadIcon} alt='upload feature icon'></img>
+            <div className='select-part'>
               <span>Select a file or drag and drop here</span>
               <input
-                id="mp3Mp4File"
+                id="mp3File"
                 type="file"
-                accept=".mp3, .mp4"
-                onChange={handleMp3Mp4FileSelect}
+                accept=".mp3"
+                onChange={handleMp3FileSelect}
               />
-              <div className="select-button">
-                <label htmlFor="mp3Mp4File">Select file</label>
-              </div>
+              <div className='select-button'><label htmlFor="mp3File">Select file</label></div>
             </div>
-            <div className="file-name">{selectedMp3Mp4FileName}</div>{" "}
-            {/* Display file name */}
+            <div className="file-name">{selectedMp3FileName}</div> {/* Display file name */}
           </div>
           <div
-            className={`three-part ${isFileSelected() ? "file-selected" : ""}`}
-            onDrop={(event) => handleDrop(event, "text1")}
+            className={`three-part ${isFileSelected() ? 'file-selected' : ''}`}
+            onDrop={(event) => handleDrop(event, 'text1')}
             onDragOver={preventDefault}
           >
-            <img
-              className="upload-icon"
-              src={uploadIcon}
-              alt="upload feature icon"
-            ></img>
-            <div className="select-part">
+            <img className='upload-icon' src={uploadIcon} alt='upload feature icon'></img>
+            <div className='select-part'>
               <span>Select a file or drag and drop here</span>
               <input
                 id="docxTxtFile1"
                 type="file"
-                accept=".docx, .txt"
+                accept=".docx, .txt, .pdf"
                 onChange={handleDocxTxtFileSelect1}
               />
-              <div className="select-button">
-                <label htmlFor="docxTxtFile1">Select file</label>
-              </div>
+              <div className='select-button'><label htmlFor="docxTxtFile1">Select file</label></div>
             </div>
-            <div className="file-name">{selectedDocxTxtFileName1}</div>{" "}
-            {/* Display file name */}
+            <div className="file-name">{selectedDocxTxtFileName1}</div> {/* Display file name */}
           </div>
           <div
-            className={`three-part ${isFileSelected() ? "file-selected" : ""}`}
-            onDrop={(event) => handleDrop(event, "text2")}
+            className={`three-part ${isFileSelected() ? 'file-selected' : ''}`}
+            onDrop={(event) => handleDrop(event, 'text2')}
             onDragOver={preventDefault}
           >
-            <img
-              className="upload-icon"
-              src={uploadIcon}
-              alt="upload feature icon"
-            ></img>
-            <div className="select-part">
+            <img className='upload-icon' src={uploadIcon} alt='upload feature icon'></img>
+            <div className='select-part'>
               <span>Select a file or drag and drop here</span>
               <input
                 id="docxTxtFile2"
                 type="file"
-                accept=".docx, .txt"
+                accept=".docx, .txt, .pdf"
                 onChange={handleDocxTxtFileSelect2}
               />
-              <div className="select-button">
-                <label htmlFor="docxTxtFile2">Select file</label>
-              </div>
+              <div className='select-button'><label htmlFor="docxTxtFile2">Select file</label></div>
             </div>
-            <div className="file-name">{selectedDocxTxtFileName2}</div>{" "}
-            {/* Display file name */}
+            <div className="file-name">{selectedDocxTxtFileName2}</div> {/* Display file name */}
           </div>
         </div>
       </div>
-      <div className="right-button">
-        <button onClick={handleUpload}>Generate</button>
-      </div>
+      <div className='right-button'><button onClick={handleUpload}>Generate</button></div>
     </div>
   );
 };
