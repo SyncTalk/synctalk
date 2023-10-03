d5deacf249afb679d373e05dafc462cac740d405
angwarrenn <ang.warrenn@gmail.com>
2023-10-03 13:36:18 +1100

    update settings

--- backend/synctalk/synctalk/settings.py
+++ backend/synctalk/synctalk/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-o752h6)+hv+wdxjoa2-cqtov(4e47!)sx6h60o*@*sl2e0zyd=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['170.64.196.122']
+ALLOWED_HOSTS = ['170.64.196.122', '127.0.0.1']
 
 
 # Application definition
