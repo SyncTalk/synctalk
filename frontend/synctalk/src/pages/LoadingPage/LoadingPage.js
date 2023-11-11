996f4ad7382ea425ff754b1a977b707d32c19d75
angwarrenn <ang.warrenn@gmail.com>
2023-11-11 22:50:06 +1100

    remove cats

--- frontend/synctalk/src/pages/LoadingPage/LoadingPage.js
+++ frontend/synctalk/src/pages/LoadingPage/LoadingPage.js
@@ -1,35 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactLoading from "react-loading";
 import "./LoadingPage.css";
 
 const Loading = () => {
-  const [url, setUrl] = useState("");
-
-  function fetchImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Request failed.");
-      })
-      .then((jsonResponse) => {
-        setUrl(jsonResponse[0].url);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  fetchImage();
-
-  setInterval(() => {
-    fetchImage();
-  }, 5000);
-
   return (
     <div className="loading">
-      <img className="infographic" src={url} alt="cat" />
+      <img className="infographic" src="" alt="cat" />
       <div className="title">
         <p>Generating</p>
         <ReactLoading type="bubbles" color="#1D3557" />
